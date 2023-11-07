ce3c0f720a2e2fd53465d4339db3c1919c2fdec4
gothe <gogogoit00@gmail.com>
2023-11-07 18:00:38 +0700

    init

---
+++ pages/api/[slug].tsx
@@ -0,0 +1,6 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const { slug } = req.query as { slug: string };
+    res.send(slug);
+}
